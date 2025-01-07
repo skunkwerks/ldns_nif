9f0ea3bbba758bc528debdacf02e14c809558c92
Dave Cottlehuber <dch@skunkwerks.at>
2025-01-07 01:15:34 +0000

    :mix new ldns

---
+++ lib/ldns.ex
@@ -0,0 +1,18 @@
+defmodule Ldns do
+  @moduledoc """
+  Documentation for `Ldns`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ldns.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ldns_test.exs
@@ -0,0 +1,8 @@
+defmodule LdnsTest do
+  use ExUnit.Case
+  doctest Ldns
+
+  test "greets the world" do
+    assert Ldns.hello() == :world
+  end
+end
