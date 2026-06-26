1a216f24551920a554f973e67d5556b6d14f849d
Dave Cottlehuber <dch@skunkwerks.at>
2026-06-26 11:59:44 +0000

    ldns:  More pretty tests, add newlines if needed, order JSON keys
    JSON keys need to be ordered, so that we can easily validate them during tests. This also aids user readability.

--- lib/ldns.ex
+++ lib/ldns.ex
@@ -18,29 +18,24 @@ defmodule LDNS do
 
   ## Examples
 
-      iex> zone = "example.com. 3600 IN SOA ns1.example.com. admin.example.com. 1 3600 1800 604800 3600\\n"
-      iex> LDNS.validate(zone)
+      iex> LDNS.validate("example.com. 3600 IN SOA ns1.example.com. admin.example.com. 1 3600 1800 604800 3600")
       :ok
 
-      # Invalid SOA record (missing required fields)
-      iex> LDNS.validate("example.com. 3600 IN SOA ns1.example.com.\\n")
+      iex> LDNS.validate("example.com. 3600 IN SOA ns1.example.com.")
       {:error, :unknown_error, 1, "Syntax error, value expected"}
 
-      # Invalid TTL value
-      iex> LDNS.validate("example.com. abc IN A 192.0.2.1\\n")
+      iex> LDNS.validate("example.com. abc IN A 192.0.2.1")
       {:error, :rdata_error, 1, "Syntax error, could not parse the RR's rdata"}
 
-      # Invalid record type
-      iex> LDNS.validate("example.com. 3600 IN INVALID 192.0.2.1\\n")
+      iex> LDNS.validate("example.com. 3600 IN INVALID 192.0.2.1")
       {:error, :rdata_error, 1, "Syntax error, could not parse the RR's rdata"}
 
-      # Invalid IP address format
-      iex> LDNS.validate("example.com. 3600 IN A 256.256.256.256\\n")
+      iex> LDNS.validate("example.com. 3600 IN A 256.256.256.256")
       {:error, :rdata_error, 1, "Syntax error, could not parse the RR's rdata"}
 
   """
-  def validate(_binary) do
-    :erlang.nif_error(:nif_not_loaded)
+  def validate(binary) when is_binary(binary) do
+    binary |> ensure_trailing_newline() |> zone_validate()
   end
 
   @doc """
@@ -53,21 +48,125 @@ defmodule LDNS do
 
   ## Examples
 
-      iex> zone = "example.org.            86400   IN      SOA ns.cabal5.net. root.example.org. 1601227221 86400 7200 3600000 1750"
-      iex> {:ok, json} = LDNS.to_json(zone)
+      iex> {:ok, json} = LDNS.to_json("example.org. 86400 IN SOA ns.cabal5.net. root.example.org. 1601227221 86400 7200 3600000 1750")
       iex> is_binary(json)
       true
   """
-  def to_json(binary) do
-    case validate(binary) do
-      :ok ->
-        case :erlang.nif_error(:nif_not_loaded) do
-          {:ok, map} -> {:ok, Jason.encode!(map)}
-          error -> error
-        end
-
-      error ->
-        error
+  def to_json(zonefile) when is_binary(zonefile) do
+    case zonefile |> ensure_trailing_newline() |> zone_to_map() do
+      {:ok, map} -> {:ok, Jason.encode!(order_zone(map), pretty: true) <> "\n"}
+      error -> error
     end
+  end
+
+  @doc ~S"""
+  to_json!/1 allows easy piping of zonefile conversion and raises on error. Yolo.
+
+  ## Examples
+
+      LDNS.to_json!("example.org. 86400 IN SOA ns.cabal5.net. root.example.org. 1601227221 86400 7200 3600000 1750") |> IO.puts()
+      {
+        "name": "example.org",
+        "records": [
+          {
+            "name": "example.org",
+            "type": "SOA",
+            "ttl": 86400,
+            "data": {
+              "mname": "ns.cabal5.net",
+              "rname": "root.example.org",
+              "serial": 1601227221,
+              "refresh": 86400,
+              "retry": 7200,
+              "expire": 3600000,
+              "minimum": 1750
+            }
+          }
+        ]
+      }
+      :ok
+
+      iex> json = LDNS.to_json!("example.org. 86400 IN SOA ns.cabal5.net. root.example.org. 1601227221 86400 7200 3600000 1750")
+      iex> Jason.decode!(json)["name"]
+      "example.org"
+  """
+  def to_json!(zonefile) when is_binary(zonefile) do
+    {:ok, map} = zonefile |> ensure_trailing_newline() |> zone_to_map()
+    Jason.encode!(order_zone(map), pretty: true) <> "\n"
+  end
+
+  @data_key_order %{
+    "SOA" => ~w(mname rname serial refresh retry expire minimum),
+    "A" => ~w(ip),
+    "AAAA" => ~w(ip),
+    "NS" => ~w(dname),
+    "PTR" => ~w(dname),
+    "CNAME" => ~w(dname),
+    "DNAME" => ~w(dname),
+    "MX" => ~w(preference exchange),
+    "TXT" => ~w(txt),
+    "SRV" => ~w(priority weight port target),
+    "NAPTR" => ~w(order preference flags service regexp replacement),
+    "DS" => ~w(key_tag algorithm digest_type digest),
+    "SSHFP" => ~w(alg fp_type fp),
+    "RRSIG" =>
+      ~w(type_covered alg labels original_ttl expiration inception key_tag signers_name signature),
+    "NSEC" => ~w(next_domain types),
+    "NSEC3" => ~w(hash_algorithm flags iterations salt next_hashed_owner types),
+    "NSEC3PARAM" => ~w(hash_algorithm flags iterations salt),
+    "DNSKEY" => ~w(flags protocol alg public_key),
+    "TLSA" => ~w(usage selector matching_type certificate_data),
+    "CAA" => ~w(flags tag value),
+    "HINFO" => ~w(cpu os),
+    "SPF" => ~w(txt),
+    "LOC" => ~w(loc)
+  }
+
+  defp order_zone(map) do
+    records = Enum.map(map["records"] || map[:records], &order_record/1)
+    Jason.OrderedObject.new([{"name", map["name"] || map[:name]}, {"records", records}])
+  end
+
+  defp order_record(rec) do
+    name = rec["name"] || rec[:name]
+    type = rec["type"] || rec[:type]
+    ttl = rec["ttl"] || rec[:ttl]
+    data = rec["data"] || rec[:data]
+    ordered_data = order_data(type, data)
+
+    Jason.OrderedObject.new([
+      {"name", name},
+      {"type", type},
+      {"ttl", ttl},
+      {"data", ordered_data}
+    ])
+  end
+
+  defp order_data(type, data) do
+    case Map.get(@data_key_order, type) do
+      nil ->
+        data
+
+      keys ->
+        pairs =
+          for k <- keys, Map.has_key?(data, k) or Map.has_key?(data, String.to_atom(k)) do
+            v = Map.get(data, k) || Map.get(data, String.to_atom(k))
+            {k, v}
+          end
+
+        Jason.OrderedObject.new(pairs)
+    end
+  end
+
+  defp ensure_trailing_newline(binary) do
+    if String.ends_with?(binary, "\n"), do: binary, else: binary <> "\n"
+  end
+
+  defp zone_validate(_binary) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  defp zone_to_map(_binary) do
+    :erlang.nif_error(:nif_not_loaded)
   end
 end
